beac265f18c8c9965e87bd75f81932d7a4df7785
zakgr <zakcorepower@gmail.com>
2020-05-06 17:01:20 +0300

    new items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React  App </h1>
+        <h1>Hi, I'm a React  App</h1>
+        <p>This is really working!!</p>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
+    //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi I\'m a React App!!!'))
   }
 }
 
